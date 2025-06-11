4d1eec3ade15717bbaa026e566b6d3fdaf912878
SevgiS <87699644+SevgiSr@users.noreply.github.com>
2025-06-11 15:29:43 +0700

    first commit
    before the filtering episode on tutorial

---
+++ api/Program.cs
@@ -0,0 +1,46 @@
+using api.Data;
+using api.Interfaces;
+using api.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+/* AddOpenApi() = same thing as AddSwaggerGen() + AddEndpointsApiExplorer() */
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ api/notes.md
@@ -0,0 +1,15 @@
+# Creating dotnet project
+
+in the project folder run:
+
+```
+dotnet new webapi -o api
+```
+
+creates dotnet inside of api folder.
+
+# watch dotnet
+
+```
+dotnet watch run
+```
