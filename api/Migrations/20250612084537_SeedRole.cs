f488817068222d939d6ca3c76a77e26725491871
SevgiS <87699644+SevgiSr@users.noreply.github.com>
2025-06-12 21:46:20 +0700

    Implemented Registration + JWT TokenService

---
+++ api/Migrations/20250612084537_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "04549452-8123-44f0-bc87-399b25e61dc3", null, "Admin", "ADMIN" },
+                    { "42484365-49bf-49a1-a8f9-6fc18910e045", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "04549452-8123-44f0-bc87-399b25e61dc3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "42484365-49bf-49a1-a8f9-6fc18910e045");
+        }
+    }
+}
---
+++ api/Models/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace api.Models
+{
+    /* IdentityUser has password and stuff already behind the scene */
+    public class AppUser : IdentityUser
+    {
+
+    }
+}
