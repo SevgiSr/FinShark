98aadfea1f4fb5cdfbaf0b30ac7cdb07bf6299c1
SevgiS <87699644+SevgiSr@users.noreply.github.com>
2025-06-16 17:02:34 +0700

    Displayed CreatedBy in the Comments

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -41,17 +41,17 @@ namespace api.Repository
 
         public async Task<List<Comment>> GetAllAsync()
         {
-            return await _context.Comments.ToListAsync();
+            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
         }
 
         public async Task<Comment?> GetByIdAsync(int id)
         {
-            return await _context.Comments.FindAsync(id);
+            return await _context.Comments.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id == id);
         }
 
         public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
         {
-            var existingComment = await _context.Comments.FindAsync(id);
+            var existingComment = await _context.Comments.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id == id);
 
             if (existingComment == null)
             {
